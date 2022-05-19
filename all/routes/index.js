de168c4d092d133478382464aefaad1642a08f40
vlap1 <maksimova.va.i20@gmail.com>
2022-05-19 09:57:18 +0300

    4.2

--- all/routes/index.js
+++ all/routes/index.js
@@ -6,4 +6,25 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница кота */
+router.get('/cat', function(req, res, next) {
+  res.send('<h1>Страница кота</h1>');
+});
+
+/* Страница Невидимки */
+router.get('/invisible', function(req, res, next) {
+  res.send('<h1>Страница Невидимки</h1>');
+});
+
+/* Страница Дракулы */
+router.get('/dracula', function(req, res, next) {
+  res.send('<h1>Страница Дракулы</h1>');
+});
+
+/* Страница Желе */
+router.get('/jelly', function(req, res, next) {
+  res.send('<h1>Страница Желе</h1>');
+});
+
+
 module.exports = router;
