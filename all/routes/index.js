cad5b29f02555c6b4cae2bac7e8b232378e11ded
vlap1 <maksimova.va.i20@gmail.com>
2022-05-19 10:33:09 +0300

    4.3

--- all/routes/index.js
+++ all/routes/index.js
@@ -8,22 +8,38 @@ router.get('/', function(req, res, next) {
 
 /* Страница кота */
 router.get('/cat', function(req, res, next) {
-  res.send('<h1>Страница кота</h1>');
+  res.render('hero', {
+  	title: "Просто кот",
+  	picture: "images/cat.jpg",
+  	desc: "Просто милый кот"
+  });
 });
 
 /* Страница Невидимки */
 router.get('/invisible', function(req, res, next) {
-  res.send('<h1>Страница Невидимки</h1>');
+  res.render('hero', {
+  	title: "Невидимка",
+  	picture: "images/invisible.jpg",
+  	desc: "Тот самый невидимый персонаж из Монстров на каникулах. Но где же его одежда, если на нем видно только очки?"
+  });
 });
 
 /* Страница Дракулы */
 router.get('/dracula', function(req, res, next) {
-  res.send('<h1>Страница Дракулы</h1>');
+  res.render('hero', {
+  	title: "Граф Дракула",
+  	picture: "images/dracula.jpg",
+  	desc: "Является одним из самых опасных монстров в мире, но вы видели его взгляд, когда он смотрит на своего внука?"
+  });
 });
 
 /* Страница Желе */
 router.get('/jelly', function(req, res, next) {
-  res.send('<h1>Страница Желе</h1>');
+  res.render('hero', {
+  	title: "Желе",
+  	picture: "images/jelly.jpg",
+  	desc: "Самый забавный и мягкий персонаж. Не раз спасал героев мультфильма от падения с большой высоты."
+  });
 });
 
 
