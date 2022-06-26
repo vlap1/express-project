16c17e663002f70ad4cd847700ba5a16df5559d9
vlap1 <maksimova.va.i20@gmail.com>
2022-06-26 23:34:19 +0300

    Сщздоние схемы

--- all/createDB.js
+++ all/createDB.js
@@ -16,10 +16,14 @@ client.close()
 })
 })*/
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/test');
-
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://0.0.0.0:27017/test')
+var schema = mongoose.Schema({ name: String })
+schema.methods.meow = function(){
+console.log(this.get("name") + " сказал мяу")
+}
+var Cat = mongoose.model('Cat', schema)
+var kitty = new Cat({ name: 'Пушок' })
+kitty.save(function (err) {
+kitty.meow()
+})
