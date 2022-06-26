b7c20b23ef12716761e246fe6fae91e67c7d0e68
vlap1 <maksimova.va.i20@gmail.com>
2022-06-27 02:12:31 +0300

    Обработка параметра в адресе

--- all/app.js
+++ all/app.js
@@ -8,6 +8,7 @@ mongoose.connect('mongodb://0.0.0.0:27017/all')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var heroes = require('./routes/heroes');
 
 var app = express();
 
@@ -26,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/heroes', heroes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
