3a4d49bb09cc3690419dd953bbac6f81dd0b5490
vlap1 <maksimova.va.i20@gmail.com>
2022-06-27 00:07:39 +0300

    Данные приложение с Mongoose

--- all/createDB.js
+++ all/createDB.js
@@ -1,45 +1,36 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://0.0.0.0:27017/all')
-var async = require("async")
 var Hero = require("./models/hero").Hero
+var async = require("async")
+var data = require('./data.js').data
 // 1. Очистить базу данных all
 // 2. Вставить 5 героев
 // 3. Закрыть соединение с базой данных
-
-
-mongoose.connection.on("open",function(){
-	var db = mongoose.connection.db
-	db.dropDatabase(function(err){
-		if(err) throw err
-
-		async.parallel([
-			function(callback){
-				var pig = new Hero({nick:"pig"})
-				pig.save(function(err,pig){
-					callback(err,"pig")
-				})
-			},
-			function(callback){
-				var vinni = new Hero({nick:"vinni"})
-				vinni.save(function(err,vinni){
-					callback(err,"vinni")
-				})
-			},
-			function(callback){
-				var sova = new Hero({nick:"sova"})
-				sova.save(function(err,sova){
-					callback(err,"sova")
-				})
-			}
-		],
-		function(err,result){
-			if(err){
-				console.log(err)
-			} else {
-				console.log("Успешно созданы герои с никами: "
-+result.join(", "))
-			}
-			mongoose.disconnect()
-		})
-	})
+async.series([
+open,
+dropDatabase,
+createHeroes,
+close
+],
+function(err,result){
+if(err) throw err
+console.log("ok")
 })
+function open(callback){
+mongoose.connection.on("open",callback)
+}
+function dropDatabase(callback){
+var db = mongoose.connection.db
+db.dropDatabase(callback)
+}
+function createHeroes(callback){
+async.each(data, function(heroData, callback){
+var hero = new mongoose.models.Hero(heroData)
+hero.save(callback)
+},
+callback)
+}
+function close(callback){
+mongoose.disconnect(callback)
+}
+
