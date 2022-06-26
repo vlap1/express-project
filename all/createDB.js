3a358cf29774356239dac4eb5a72a3cc09bc72f9
vlap1 <maksimova.va.i20@gmail.com>
2022-06-27 00:00:47 +0300

    Заполнение базы данных с Mongoose

--- all/createDB.js
+++ all/createDB.js
@@ -1,12 +1,45 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://0.0.0.0:27017/all')
+var async = require("async")
 var Hero = require("./models/hero").Hero
-var hero = new Hero({
-title: "Пятачок",
-nick: "pig"
-})
-console.log(hero)
-hero.save(function(err, hero, affected){
-console.log(hero.title)
-})
+// 1. Очистить базу данных all
+// 2. Вставить 5 героев
+// 3. Закрыть соединение с базой данных
 
+
+mongoose.connection.on("open",function(){
+	var db = mongoose.connection.db
+	db.dropDatabase(function(err){
+		if(err) throw err
+
+		async.parallel([
+			function(callback){
+				var pig = new Hero({nick:"pig"})
+				pig.save(function(err,pig){
+					callback(err,"pig")
+				})
+			},
+			function(callback){
+				var vinni = new Hero({nick:"vinni"})
+				vinni.save(function(err,vinni){
+					callback(err,"vinni")
+				})
+			},
+			function(callback){
+				var sova = new Hero({nick:"sova"})
+				sova.save(function(err,sova){
+					callback(err,"sova")
+				})
+			}
+		],
+		function(err,result){
+			if(err){
+				console.log(err)
+			} else {
+				console.log("Успешно созданы герои с никами: "
++result.join(", "))
+			}
+			mongoose.disconnect()
+		})
+	})
+})
