e73beb62cf574ca461eb1b0b2a096bca1d5d84cd
vlap1 <maksimova.va.i20@gmail.com>
2022-06-26 23:15:34 +0300

    Async и enbkbnf Apply

--- all/file.js
+++ all/file.js
@@ -1,17 +1,18 @@
 var async = require("async")
 var from_one_two_five = [1,"два",3,4,5]
-async.waterfall([
-function(callback){
-callback(null, "МАМА", "МЫЛА","РАМУ")
-},
-function(arg1, arg2, arg3, callback){
-callback(null, arg1 + ' ' + arg2 + ' ' + arg3)
-}
+async.series([
+async.apply(anyWord, "МАМА"),
+async.apply(anyWord, "МЫЛА"),
+async.apply(anyWord, "РАМУ")
 ],
-function(err, result){
-if(err) throw err
-console.log(result)
+function(err,result){
+if(err) throw err;
+console.log(result.join(' '))
 });
+function anyWord(arg, callback){
+callback(null, arg)
+}
+
 
 
 
