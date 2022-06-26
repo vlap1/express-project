d6710b2672860485afca5ce766b2b18f9b1a763b
vlap1 <maksimova.va.i20@gmail.com>
2022-06-27 02:20:27 +0300

    Чистка кода

--- all/routes/heroes.js
+++ all/routes/heroes.js
@@ -9,15 +9,15 @@ router.get('/', function(req, res, next) {
 
 /* Страница героев */
 router.get('/:nick', function(req, res, next) {
-Hero.findOne({nick:req.params.nick}, function(err,hero){
-if(err) return next(err)
-if(!hero) return next(new Error("Нет такого героя в этой книжке"))
-res.render('hero', {
-title: hero.title,
-picture: hero.avatar,
-desc: hero.desc
-})
-})
+	Hero.findOne({nick:req.params.nick}, function(err,hero){
+		if(err) return next(err)
+		if(!hero) return next(new Error("Нет такого героя в этой книжке"))
+		res.render('hero', {
+			title: hero.title,
+			picture: hero.avatar,
+			desc: hero.desc
+		})
+	})
 })
 
 module.exports = router;
--- all/routes/index.js
+++ all/routes/index.js
@@ -16,31 +16,31 @@ router.get('/cat', function(req, res, next) {
 });*/
 
 /* Страница Невидимки */
-router.get('/invisible', function(req, res, next) {
+/*router.get('/invisible', function(req, res, next) {
   res.render('hero', {
   	title: "Невидимка",
   	picture: "images/invisible.jpg",
   	desc: "Тот самый невидимый персонаж из Монстров на каникулах. Но где же его одежда, если на нем видно только очки?"
   });
-});
+});*/
 
 /* Страница Дракулы */
-router.get('/dracula', function(req, res, next) {
+/*router.get('/dracula', function(req, res, next) {
   res.render('hero', {
   	title: "Граф Дракула",
   	picture: "images/dracula.jpg",
   	desc: "Является одним из самых опасных монстров в мире, но вы видели его взгляд, когда он смотрит на своего внука?"
   });
-});
+});*/
 
 /* Страница Желе */
-router.get('/jelly', function(req, res, next) {
+/*router.get('/jelly', function(req, res, next) {
   res.render('hero', {
   	title: "Желе",
   	picture: "images/jelly.jpg",
   	desc: "Самый забавный и мягкий персонаж. Не раз спасал героев мультфильма от падения с большой высоты."
   });
-});
+});*/
 
 
 module.exports = router;
