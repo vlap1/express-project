a58c3260eef96ab552e4e4e34d40dc3813511430
vlap1 <maksimova.va.i20@gmail.com>
2022-06-26 23:08:13 +0300

    Async и шаблон Series

--- all/file.js
+++ all/file.js
@@ -1,20 +1,21 @@
 var async = require("async")
 var from_one_two_five = [1,"два",3,4,5]
-async.each(
-from_one_two_five,
-function(elem,report){
-if(typeof elem === "number"){
-report()
-} else {
-report("Нашли не число")
+async.series([
+firstWord,
+secondWord,
+thirdWord
+],
+function(err,result){
+if(err) throw err;
+console.log(result.join(' '))
+});
+function firstWord(callback){
+callback(null, 'МАМА')
 }
-},
-function(info){
-if(info){
-console.log(info)
-} else {
-console.log("Проверка прошла успешно. Все элементы массива являются числами")
+function secondWord(callback){
+callback(null, 'МЫЛА')
 }
+function thirdWord(callback){
+callback(null, 'РАМУ')
 }
-)
 
