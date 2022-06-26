e34f8ac54dac8968d7970d4c8113c67a91bdf4cf
vlap1 <maksimova.va.i20@gmail.com>
2022-06-26 23:31:49 +0300

    ORM

--- all/createDB.js
+++ all/createDB.js
@@ -1,4 +1,4 @@
-var MongoClient = require("mongodb").MongoClient
+/*var MongoClient = require("mongodb").MongoClient
 var data = require("./data.js").data
 
 MongoClient.connect("mongodb://0.0.0.0:27017/all",
@@ -14,4 +14,12 @@ var collection = db.collection("heroes")
 collection.insertMany(data,function(err,result){
 client.close()
 })
-})
+})*/
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://0.0.0.0:27017/test');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
