a9d15f24030954f9a92589e78e06b355c6eacbd7
vlap1 <maksimova.va.i20@gmail.com>
2022-06-27 02:36:37 +0300

    Заполнение меню из базы данных

--- all/routes/index.js
+++ all/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Hero = require("../models/hero").Hero
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Hero.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+            title: 'Express',
+            menu: menu
+        });
+    })
 });
 
 /* Страница кота 
