facc3f005a10173edddbe96a5f53fc034e6fd24c
vlap1 <maksimova.va.i20@gmail.com>
2022-06-26 23:40:40 +0300

    Создание модели данных приложения

--- all/createDB.js
+++ all/createDB.js
@@ -1,29 +1,12 @@
-/*var MongoClient = require("mongodb").MongoClient
-var data = require("./data.js").data
-
-MongoClient.connect("mongodb://0.0.0.0:27017/all",
-function(err,client) {
-if(err) {
-	throw err;
-}
-
-const db = client.db('all');
-db.dropDatabase()
-
-var collection = db.collection("heroes")
-collection.insertMany(data,function(err,result){
-client.close()
-})
-})*/
-
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://0.0.0.0:27017/test')
-var schema = mongoose.Schema({ name: String })
-schema.methods.meow = function(){
-console.log(this.get("name") + " сказал мяу")
-}
-var Cat = mongoose.model('Cat', schema)
-var kitty = new Cat({ name: 'Пушок' })
-kitty.save(function (err) {
-kitty.meow()
+mongoose.connect('mongodb://0.0.0.0:27017/all')
+var Hero = require("./models/hero").Hero
+var hero = new Hero({
+title: "Пятачок",
+nick: "pig"
 })
+console.log(hero)
+hero.save(function(err, hero, affected){
+console.log(hero.title)
+})
+
